597b7d58f321674f14da41a7542b6a3643fb9d82
rahmou1 <ahmedrahmou11694@gmail.com>
2022-07-11 15:25:25 +0200

    adding security and morgan

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import ratelimit from 'express-rate-limit';
 
 const PORT = 3000;
 //* Create instance server
 const app: Application = express();
+//* Middleware to parse incoming requests
+app.use(express.json());
 
+//* HTTP request logger middleware
+app.use(morgan('common'));
+//* HTTP security middleware
+app.use(helmet());
+//* Apply Rate limiting middleware to all requests
+app.use(
+  ratelimit({
+    windowMs: 60 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: 'Too many requests from this IP, please try again after an hour',
+  })
+);
 //* add routing for/path
 app.get('/', (req: Request, res: Response) => {
   res.json({ status: 'true', message: 'Hello World ❤️👀' });
